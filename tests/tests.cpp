199adc9e50709f3fcd64665cd8667da62d731532
Kojirion <kojirion@gmail.com>
2014-04-22 00:41:59 +0100

    re-introduced tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,3 +1,11 @@
-int main(){
-    return 0;
+#define BOOST_TEST_MAIN
+#include <boost/test/included/unit_test.hpp>
+#include "../BlackWolf.hpp"
+
+BOOST_AUTO_TEST_CASE(parsing)
+{
+    Move move{{0,0}, {1,1}};
+    Move move_2 = move;
+
+    BOOST_CHECK(true);
 }
