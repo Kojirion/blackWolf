05ca14cc542e2d37b485fd190bc7600bc99fab85
Kojirion <kojirion@gmail.com>
2014-09-13 22:43:28 +0100

    timeseal implemented, not used yet

--- Client.cpp
+++ Client.cpp
@@ -4,6 +4,7 @@
 #include "parsers/GameStartParser.hpp"
 #include "parsers/GameEndParser.hpp"
 #include <boost/fusion/container/vector.hpp>
+#include "Timeseal.hpp"
 
 Client::Client():
     outputStream(&output),
@@ -17,30 +18,37 @@ void Client::connect()
     boost::asio::ip::tcp::resolver::query query("freechess.org","5000");
     boost::asio::ip::tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
     boost::asio::ip::tcp::resolver::iterator end;
-
+    
     boost::system::error_code error = boost::asio::error::host_not_found;
-
+    
     while(error && endpoint_iterator != end)
     {
         socket.close();
         socket.connect(*endpoint_iterator++, error);
     }
-
+    
     if (error)
     {
         textReady("Failed to connect.");
         std::cerr << "Failed to connect." << std::endl;
         return;
     }
+    
+    //std::string hello = "TIMESTAMP|openseal|Running on an operating system|";
+    //toClient(hello);
 
+//    int n=crypt(hello);
+//    toClient(hello);
+    
     boost::asio::async_read_until(socket, data, "\n\r",
                                   boost::bind(&Client::handleData, this, _1));
+
 }
 
 void Client::update()
 {
     boost::asio::write(socket, output);
-
+    
     if (!io_service.poll())
         io_service.reset();
 }
@@ -51,18 +59,18 @@ void Client::handleData(boost::system::error_code ec)
     {
         std::istream is(&data);
         std::string str;
-        while(std::getline(is, str)){
-
+        while(std::getline(is, str)){          
+            
             boost::erase_all(str,"\r");
-
+            
             using boost::spirit::qi::parse;
-
+            
             static const GameEndParser gameEndParser;
             static const GameStartParser gameStartParser;
             static const SessionStartParser sessionStartParser;
             static const GameStateParser gameStateParser;
             GameStateTuple gameState;
-
+            
             if (parse(str.begin(), str.end(), gameStateParser, gameState)){
                 using boost::fusion::at_c;
                 int white_time = at_c<1>(gameState);
@@ -70,7 +78,7 @@ void Client::handleData(boost::system::error_code ec)
                 positionReady(at_c<0>(gameState), white_time, black_time);
             }else{
                 GameStartTuple gameStart;
-
+                
                 if (parse(str.begin(), str.end(), gameStartParser, gameStart)){
                     using boost::fusion::at_c;
                     int time = 60*at_c<2>(gameStart);
@@ -95,7 +103,7 @@ void Client::handleData(boost::system::error_code ec)
                 }
             }
         }
-
+        
         boost::asio::async_read_until(socket, data, "\n\r",
                                       boost::bind(&Client::handleData, this, _1));
     }
