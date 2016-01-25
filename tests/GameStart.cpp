6786e0685c57e25da456678da3ca961150404047
Kojirion <kojirion@gmail.com>
2016-01-25 01:08:51 +0000

    Add checks in game start unit test

--- tests/GameStart.cpp
+++ tests/GameStart.cpp
@@ -8,6 +8,12 @@ BOOST_AUTO_TEST_CASE(ParsingGameStart)
 {
     std::string given = "Creating: Kojijay (++++) Thulius (1690) unrated blitz 2 12";
 
-    auto success = parse(given.begin(), given.end(), GameStartParser());
+    GameStartParser gameStartParser;
+    NewGameMessage newGameMessage;
+
+    auto success = parse(given.begin(), given.end(), gameStartParser, newGameMessage);
     BOOST_CHECK(success);
+    BOOST_CHECK_EQUAL(newGameMessage.p1, "Kojijay");
+    BOOST_CHECK_EQUAL(newGameMessage.p2, "Thulius");
+    BOOST_CHECK_EQUAL(newGameMessage.time, 2);
 }
